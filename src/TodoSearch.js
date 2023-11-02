774ebb103b545e1e6d64735af113987df9dfdac7
Will Luna <willluna.zac.com@gmail.com>
2023-11-02 18:42:21 -0500

    08-Se creo un estado derivado para Contar los Todos y totales.

--- src/TodoSearch.js
+++ src/TodoSearch.js
@@ -1,11 +1,7 @@
 import React from "react";
 import "./TodoSearch.css";
 
-function TodoSearch() {
-  const [searchValue, setSearchValue] = React.useState("");
-
-  console.log("Los usuarios buscan todos de  " + searchValue);
-
+function TodoSearch({ searchValue, setSearchValue }) {
   return (
     <input
       placeholder="Cortar Cebolla"
