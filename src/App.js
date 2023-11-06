65e8eeda46aa534dc314deeb40e18468f68e41c5
Will Luna <willluna.zac.com@gmail.com>
2023-11-06 14:49:38 -0500

    10- se creo las funciones que permiten completar o eliminar todos.

--- src/App.js
+++ src/App.js
@@ -25,7 +25,19 @@ function App() {
     return todoText.includes(searchText);
   });
 
-  console.log("Los usuarios buscan todos de " + searchValue);
+  const completeTodo = (text) => {
+    const newTodos = [...todos];
+    const todoIndex = newTodos.findIndex((todo) => todo.text == text);
+    newTodos[todoIndex].completed = true;
+    setTodos(newTodos);
+  };
+
+  const deleteTodo = (text) => {
+    const newTodos = [...todos];
+    const todoIndex = newTodos.findIndex((todo) => todo.text == text);
+    newTodos.splice(todoIndex, 1);
+    setTodos(newTodos);
+  };
 
   return (
     <>
@@ -38,6 +50,8 @@ function App() {
             key={todo.text}
             text={todo.text}
             completed={todo.completed}
+            onComplete={() => completeTodo(todo.text)}
+            onDelete={() => deleteTodo(todo.text)}
           />
         ))}
       </TodoList>
